80bfdb8faff430c0c504069091a43cffcc481876
Dimaslv2 <muhammaddimaslv2@gmail.com>
2023-09-30 03:44:11 +0700

    Use binding expressions (Step 9)

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -71,16 +71,7 @@ class GameFragment : Fragment() {
         // Observe the scrambledCharArray LiveData, passing in the LifecycleOwner and the observer.
         viewModel.currentScrambledWord.observe(viewLifecycleOwner,
             { newWord ->
-                binding.textViewUnscrambleWord.text = newWord
-            })
-        viewModel.score.observe(viewLifecycleOwner,
-            { newScore ->
-                binding.score.text = getString(R.string.score, newScore)
-            })
-        viewModel.currentWordCount.observe(viewLifecycleOwner,
-            { newWordCount ->
-                binding.wordCount.text =
-                    getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+                binding.textViewUnscrambledWord.text = newWord
             })
     }
 
